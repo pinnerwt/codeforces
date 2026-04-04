a10e4e3d4bfee80597e605194d4e6a2d3547886c
pinnertw <wayne567tw@yahoo.com.tw>
2026-04-04 16:27:25 +0000

    Answers

--- 2218/d/sol.cpp
+++ 2218/d/sol.cpp
@@ -5,7 +5,33 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    const int MAXP = 110000;
+    vector<bool> is_prime (MAXP, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i=2; i * i < MAXP; i++) {
+      if (is_prime[i]) {
+        for (int j = i*i; j < MAXP; j+= i) {
+          is_prime[j] = false;
+        }
+      }
+    }
+    vector<long long> p;
+    for (int i=2; i<MAXP; ++i) {
+      if (is_prime[i]) p.push_back(i);
+    }
 
-
+    int t;
+    int n;
+    cin >> t;
+    while (t--) {
+      cin >> n;
+      for (int i=0; i < n; ++i) {
+        if (i) cout << ' ';
+        if (i == 0) cout << p[0];
+        else if (i == n - 1) cout << p[n-2];
+        else cout << p[i - 1] * p[i];
+      }
+      cout << '\n';
+    }
     return 0;
 }
--- 2218/e/sol.cpp
+++ 2218/e/sol.cpp
@@ -5,7 +5,51 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int t;
+    int n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        vector<int> a(n);
+        for (int i=0; i < n; ++i) {
+          cin >> a[i];
+        }
 
+        // Binary trie for maximum XOR pair
+        vector<array<int,2>> tr(1, {-1, -1});
+
+        auto insert = [&](int x) {
+            int u = 0;
+            for (int b = 29; b >= 0; b--) {
+                int bit = (x >> b) & 1;
+                if (tr[u][bit] == -1) {
+                    tr[u][bit] = tr.size();
+                    tr.push_back({-1, -1});
+                }
+                u = tr[u][bit];
+            }
+        };
+
+        auto query = [&](int x) -> int {
+            int u = 0, res = 0;
+            for (int b = 29; b >= 0; b--) {
+                int bit = (x >> b) & 1;
+                if (tr[u][1 - bit] != -1) {
+                    res |= 1 << b;
+                    u = tr[u][1 - bit];
+                } else {
+                    u = tr[u][bit];
+                }
+            }
+            return res;
+        };
+
+        for (int x : a) insert(x);
+
+        int ans = 0;
+        for (int x : a) ans = max(ans, query(x));
+        cout << ans << '\n';
+    }
 
     return 0;
 }
